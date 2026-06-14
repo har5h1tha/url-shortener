73181817f80ebcbb98111b70515d0d282e461f36
Harshitha D <harshitha7d@gmail.com>
2026-06-14 20:45:26 +0530

    feat: add frontend UI

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
-import express from 'express';
-import urlRouter from './routers/url.router.js';
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import urlRouter from './routers/url.router.js'
 import errorHandler from './middlewares/error.middleware.js'
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-const app=express();
+const app = express()
 
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '../public')))  // serve frontend
+app.use('/', urlRouter)
+app.use(errorHandler)
 
-app.use('/',urlRouter)
-app.use(errorHandler) 
-
-export default app;
+export default app
